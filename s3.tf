d91c49772ebb525a9ab00c6605e86cfc34cc74ee
Vibhu Yadav <vibhu.yadav@newsela.com>
2022-08-08 16:10:46 -0400

    Demo-0 test

--- s3.tf
+++ s3.tf
@@ -1,3 +1,6 @@
-resource "aws_s3_bucket" "aws_s3_bucket" {
-  bucket = "vibhu-env0-poc-bucket"
+
+module "demo_s3_bucket" {
+  source = "api.env0.com/1e2678b2-d823-40ea-a5fa-e6479ae0750e/terraform-aws-s3/aws"
+  version = "0.0.1"
+  bucket_name = "demo-vibhu-s3-bucket"
 }
